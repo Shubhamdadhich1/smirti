53734811081ab79076da5e4f8f7672bffd59687c
Shubham Dadhich <dadhichshubham181@gmail.com>
2026-04-25 19:04:55 +0530

    Add Twilio error logging

--- backend/services/twilioService.js
+++ backend/services/twilioService.js
@@ -7,6 +7,7 @@ const { todayStr } = require('../utils/timeUtils');
  * @param {Object} rem - The reminder object
  */
 function makeCall(rem) {
+  console.log(`[Twilio] Attempting to call ${rem.parentPhone} for reminder ${rem.id}...`);
   client.calls.create({
     to: rem.parentPhone,
     from: TWILIO_NUMBER,
@@ -14,7 +15,9 @@ function makeCall(rem) {
     statusCallback: `${BASE_URL}/status?id=${rem.id}`,
     statusCallbackEvent: ['completed'],
     statusCallbackMethod: 'POST'
-  });
+  })
+  .then(call => console.log(`[Twilio] Call initiated successfully! Call SID: ${call.sid}`))
+  .catch(err => console.error(`[Twilio] Error initiating call:`, err));
 }
 
 /**
